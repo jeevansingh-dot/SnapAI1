38aeb17ae0057133c81cf23b4370e940bba3532a
jeevan singh <mantupanora@gmail.com>
2026-09-19 08:50:06 +0530

    Update History.jsx

--- frontend/src/History.jsx
+++ frontend/src/History.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
+  const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/history`;
 function History() {
   const [historyItems, setHistoryItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
-  const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/history`;
+  //const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/history`;
   // ==========================================
   // FETCH HISTORY
   // ==========================================
